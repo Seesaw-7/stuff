118af193577f92f74c79119923482d70d77603e6
Seesaw <weiseesaw@gmail.com>
2024-09-04 16:56:55 -0400

    impv: 80

--- 80_remove_duplicates_from_sorted_array_II.py
+++ 80_remove_duplicates_from_sorted_array_II.py
@@ -1,5 +1,6 @@
 class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
+    # My sol: use a flag to flag whether the second occurance of a number has occurred
+    def removeDuplicates1(self, nums: list[int]) -> int:
         k = 1 if len(nums) else 0
         flag = 0
         for i in range(1, len(nums)):
@@ -13,3 +14,14 @@ class Solution:
                     k += 1
                     flag = 1
         return k
+
+    # Compare nums[i] with nums[k-2], where k signals the current valid sequence. 
+    def removeDuplicates(self, nums: list[int]) -> int:
+        k = 1 if len(nums) else 0
+        k = 2 if len(nums) > 1 else k
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k-2]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+        
