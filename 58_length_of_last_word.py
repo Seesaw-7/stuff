ce6a6cf50ea653f669b5612e6ebb709948486bbe
Seesaw <weiseesaw@gmail.com>
2024-09-20 11:59:57 -0400

    58

--- 58_length_of_last_word.py
+++ 58_length_of_last_word.py
@@ -1,5 +1,23 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
+    # with built-in
+    def lengthOfLastWord1(self, s: str) -> int:
         words = s.strip().split()
         return len(words[-1])
         
+    # do not use built-in
+    def lengthOfLastWord(self, s: str) -> int:
+        flag = 0
+        cnt = 0
+
+        for c in reversed(s):
+            if c != ' ':
+                if not flag:
+                    flag = 1
+                    cnt = 1
+                else:
+                    cnt += 1
+            else:
+                if flag:
+                    return cnt
+
+        return cnt
