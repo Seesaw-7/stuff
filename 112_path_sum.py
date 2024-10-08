ed73700067b796812561515e8dd3516700f9b20f
Seesaw <weiseesaw@gmail.com>
2024-10-08 16:13:21 -0400

    112

--- 112_path_sum.py
+++ 112_path_sum.py
@@ -26,3 +26,11 @@ class Solution:
         if not root:
             return False
         return targetSum == helper(root, 0, targetSum)
+
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+            if not root:
+                return False
+            targetSum -= root.val
+            if not root.left and not root.right:
+                return targetSum == 0
+            return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
